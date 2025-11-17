329b91ed60d678ff82bb19bd365d16d398848dc7
Adrian Loenan <aloenan01.qub.ac.uk>
2025-11-17 20:23:01 +0000

    Part_1_unicafe

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,45 +1,63 @@
-const Part = (props) =>{
-  console.log(props)
-  return (
-    <div>
-      <p>{props.part} {props.total}</p>
-    </div>
-  ) 
-}
-const Header = (props) =>{
-  console.log(props)
-  return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
+import { useState } from 'react'
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const Stats = ({text, value}) => {
+  if(text === "positive"){
+    return <p>{text}: {value}%</p>
+  } else {
+    return <p>{text}: {value}</p>
+  }
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [totalClick, setTotalClick] = useState(0)
+  const [totalPoint, setTotalPoint] = useState(0)
+
+  const goodReview = () => {
+    setGood(good + 1)
+    setTotalClick(totalClick + 1)
+    setTotalPoint(totalPoint + 1)
   }
+
+  const neutralReview = () => {
+    setNeutral(neutral + 1)
+    setTotalClick(totalClick + 1)
+  }
+  const badReview = () => {
+    setBad(bad + 1)
+    setTotalClick(totalClick + 1)
+    setTotalPoint(totalPoint - 1)
+  }
+
+  const average = totalClick === 0 ? 0 : totalPoint / totalClick
+  const positive = totalClick === 0 ? 0 : (good / totalClick) * 100
 
   return (
     <div>
-      <Header course={course.name}/>
-      <Part part={course.parts[0].name} total={course.parts[0].exercises}/>
-      <Part part={course.parts[1].name} total={course.parts[1].exercises}/>
-      <Part part={course.parts[2].name} total={course.parts[2].exercises}/>
+      <h1>give feedback</h1>
+      <Button onClick={goodReview} text='good'/>
+      <Button onClick={neutralReview} text='neutral'/>
+      <Button onClick={badReview} text='bad'/>
+
+      <h1>Statistics</h1>
+
+      {totalClick === 0? (
+        <p>No feedback given</p>
+      ): (
+        <>
+          <Stats text='good' value={good}/>
+          <Stats text='neutral' value={neutral}/>
+          <Stats text='bad' value={bad}/>
+          <Stats text='all' value={totalClick}/>
+          <Stats text='average' value={average}/>
+          <Stats text='positive' value={positive}/>
+        </>
+      )}
     </div>
   )
 }
