a5fef6279fd02db8be9a4d183c1b925120229f06
Adrian Loenan <aloenan01.qub.ac.uk>
2025-12-03 14:31:34 +0000

    Render test

--- Part3/index.js
+++ Part3/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
 
@@ -69,8 +72,8 @@ app.post(`/api/notes`, (request, response) =>{
   }
 
   const note = {
-    content: body-content,
-    important: body.importatnt || false,
+    content: body.content,
+    important: body.important || false,
     id: generateId(),
   }
 
@@ -84,7 +87,7 @@ const unknownEndpoint = (request, response) =>{
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
